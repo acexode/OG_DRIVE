37b2dd4cd6b3baba4671fefda03e57bdc4e4177f
Acexode <abubakar.abdulwahab@outsourceglobal.com>
2020-05-01 07:09:37 +0100

    set for prod

--- index.js
+++ index.js
@@ -38,11 +38,10 @@ mongoose.connect(  process.env.MONGODBURI,{
   app.use('/api', routes)
 
   if(process.env.NODE_ENV === 'production'){
-    app.use(express.static(path.join(__dirname, 'client/build')));
-    app.get('*', (req,res) =>{
-      res.sendFile(path.join(__dirname+'/client/build/index.html'));
-  });
-
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
   }
   const port = process.env.PORT || 5000
 
